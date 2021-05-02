c317a0a7b87ca0040e1aa63609e3ae8b04b1d8f1
bea <beamaia.work@gmail.com>
2021-05-02 16:48:50 -0300

    muda mais de lugar e testa leitura

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "../include/Candidato.h"
+#include "../include/DateUtils.h"
+#include "../include/Erros.h"
+#include "../include/Leitura.h"
+#include "../include/Partido.h"
+#include "../include/Pessoa.h"
+#include "../include/Votos.h"
+
+int main (int argc, char *argv[]) {
+    lerPartidos(argv[2]);
+    return 0;
+}
