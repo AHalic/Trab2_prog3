9d4d9451027b86c2c0f24858e163dca9852c37de
bea <beamaia.work@gmail.com>
2021-05-02 22:44:59 -0300

    adiciona leitura de partidos e candidatos

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 #include "../include/Candidato.h"
 #include "../include/DateUtils.h"
@@ -9,6 +10,25 @@
 #include "../include/Votos.h"
 
 int main (int argc, char *argv[]) {
-    lerPartidos(argv[2]);
+    string caminhoCandidato = argv[1];
+    string caminhoPartido = argv[2];
+    string dataStr = argv[3];
+
+    cout << caminhoPartido << endl;
+    try {
+        vector<Partido*> partidos = lePartidos(caminhoPartido);
+        
+        if (partidos.empty())
+            throw domain_error("Partidos vazios");
+        
+        vector<Candidato*> candidatos = leCandidatos(caminhoCandidato);
+
+        if (candidatos.empty()) 
+            throw domain_error("Candidatos vazios");
+
+    } catch (exception &e) {
+        cerr << e.what() << endl;
+        return 1;
+    }
     return 0;
 }
