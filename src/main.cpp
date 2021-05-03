5ec7a0694cf2b6cca97de2e3443bd3b0ebbf6d45
bea <beamaia.work@gmail.com>
2021-05-03 17:39:21 -0300

    adiciona eleicao

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,45 @@
 #include <iostream>
 #include <vector>
 
+#include "../include/Votos.h"
+#include "../include/Pessoa.h"
 #include "../include/Candidato.h"
+#include "../include/Partido.h"
+#include "../include/Eleicao.h"
+#include "../include/Leitura.h"
 #include "../include/DateUtils.h"
 #include "../include/Erros.h"
-#include "../include/Leitura.h"
-#include "../include/Partido.h"
-#include "../include/Pessoa.h"
-#include "../include/Votos.h"
 
+using namespace std;
 using namespace cpp_util;
 
 int main (int argc, char *argv[]) {
-    /*
     string caminhoCandidato = argv[1];
     string caminhoPartido = argv[2];
-    string dataStr = argv[3];
+    string dataEleicaoStr = argv[3];
+    
+    time_t dataEleicao;
+    vector<Partido*> partidos;
+    vector<Candidato*> candidatos;
 
-    cout << caminhoPartido << endl;
     try {
-        vector<Partido*> partidos = lePartidos(caminhoPartido);
+        if(!validDate(dataEleicaoStr)) {
+            throw runtime_error("Erro de parsing de string para time_t.");
+        }
+
+        dataEleicao = parseDate(dataEleicaoStr);
+    } catch (exception &e) {
+        cerr << e.what() << endl;
+        return 1;
+    }
+
+    try {
+        partidos = lePartidos(caminhoPartido);
         
         if (partidos.empty())
             throw domain_error("Partidos vazios");
-
-        for (Partido* aux : partidos) {
-            cout << aux << endl;
-        }
         
-        vector<Candidato*> candidatos = leCandidatos(caminhoCandidato);
+        candidatos = leCandidatos(caminhoCandidato, partidos);
 
         if (candidatos.empty()) 
             throw domain_error("Candidatos vazios");
@@ -37,18 +48,22 @@ int main (int argc, char *argv[]) {
         cerr << e.what() << endl;
         return 1;
     }
-    */
 
+    Eleicao* eleicao = new Eleicao(dataEleicao, partidos, candidatos);
+    cout << "Sobre a eleicao:" << endl;
+    cout << "Vagas: " << eleicao->getVagas() << endl;
+    cout << "F: " << eleicao->getFEleitas() << endl;
+    cout << "Votos: " << eleicao->getVotosTotais() << endl;
 
     // Teste de Pessoa
-    time_t niver = parseDate("22/07/1998");
-    time_t hoje = parseDate("03/05/2021");
-    Partido* p = new Partido("Partido da Sophie", "SP", 3, 1);
-    Candidato* c = new Candidato("Sophie", 'F', niver, "Eleito", "Halic", 35, 11111, "Eleito", p);
+    // time_t niver = parseDate("22/07/1998");
+    // time_t hoje = parseDate("03/05/2021");
+    // Partido* p = new Partido("Partido da Sophie", "SP", 3, 1);
+    // Candidato* c = new Candidato("Sophie", 'F', niver, "Eleito", "Halic", 35, 11111, "Eleito", p);
     // Pessoa* p = new Pessoa("bea maia", 'F', niver);
     // cout << "pessoa: "<< p << endl;
     // cout << p->calculaIdade(hoje) << endl;
-    cout << c << endl;
+    // cout << c << endl;
 
     return 0;
 }
