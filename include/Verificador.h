e9c6c5113d9bfc2eaacfca484fdd479b719cdec5
bea <beamaia.work@gmail.com>
2021-05-02 23:18:38 -0300

    adiciona funções de verificação

---
+++ include/Verificador.h
@@ -0,0 +1,17 @@
+#ifndef VERIFICADOR_H
+#define VERIFICADOR_H
+
+#include <string>
+
+using namespace std;
+
+bool linhaCandidatoValida (string numeroIdStr, string votosNominaisStr, string situacao, string nome, string nomeUrna, string genero, string dataNascStr, string destinoVoto, string numeroPartidoStr);
+bool linhaPartidoValida (string numeroPartidoStr, string votosLegendaStr, string nome, string sigla);
+bool CandidatoHeaderValido(string linha);
+bool PartidoHeaderValido(string linha);
+bool ehPositivo(int valor);
+
+bool generoValido(string genero);
+
+
+#endif
---
+++ src/Verificador.cpp
@@ -0,0 +1,70 @@
+#include "../include/Verificador.h"
+
+#include <string>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+bool linhaCandidatoValida (string numeroIdStr, string votosNominaisStr, string situacao, string nome, string nomeUrna, string genero, string dataNascStr, string destinoVoto, string numeroPartidoStr) {
+    if (numeroIdStr.empty() || votosNominaisStr.empty() || situacao.empty() || nome.empty() || nomeUrna.empty() ||
+        genero.empty() || dataNascStr.empty() || destinoVoto.empty() || numeroPartidoStr.empty()) {
+        return false;
+    }
+
+    return true;
+}
+
+bool linhaPartidoValida (string numeroPartidoStr, string votosLegendaStr, string nome, string sigla) {
+    if (numeroPartidoStr.empty() || votosLegendaStr.empty() || nome.empty() || sigla.empty()) {
+        return false;
+    }
+
+    return true;
+}
+
+bool CandidatoHeaderValido(string linha) {
+    int contador = 0;
+    string aux;
+    stringstream s(linha);
+
+    while(getline(s, aux, ',')) {
+        contador++;
+    }
+
+    cout << "cand: " << contador << endl;
+
+    if(contador == 9)
+        return true;
+    
+    return false;
+}
+
+bool PartidoHeaderValido(string linha) {
+    int contador = 0;
+    string aux;
+    stringstream s(linha);
+
+    while(getline(s, aux, ',')) {
+        contador++;
+    }
+
+    cout << "par: " << contador << endl;
+
+    if(contador == 4)
+        return true;
+    
+    return false;
+}
+
+bool ehPositivo(int valor) {
+    if (valor >= 0)
+        return true;
+    return false;
+}
+
+bool generoValido(string genero) {
+    if(!genero.compare("M") || !genero.compare("F"))
+        return true;
+    return false;
+}
