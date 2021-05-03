ea11a0a075722658062dd31b11fe5ce87ec0d9d2
bea <beamaia.work@gmail.com>
2021-05-02 23:47:58 -0300

    adiciona classe votos

--- include/Votos.h
+++ include/Votos.h
@@ -2,7 +2,21 @@
 #define VOTOS_H
 
 class Votos {
-    
+    private:
+        int votosNominais;
+        int votosLegenda;
+        int votosTotais;
+
+    public:
+        Votos(int votosLegenda);
+        Votos();
+
+        int getVotosLegenda() const;
+        int getVotosNominais() const;
+        int getVotosTotais() const;
+
+        void setVotosLegenda(int votos);
+        void setVotosNominais(int votos);
 };
 
 #endif
--- src/Votos.cpp
+++ src/Votos.cpp
@@ -1,3 +1,37 @@
 #include "../include/Votos.h"
 
 using namespace std;
+
+Votos::Votos(int votosLegenda) {
+    this->votosLegenda = votosLegenda;
+    this->votosNominais = 0;
+    this->votosTotais = votosLegenda;
+}
+
+Votos::Votos() {
+    this->votosLegenda = 0;
+    this->votosNominais = 0;
+    this->votosTotais = 0;
+}
+
+int Votos::getVotosLegenda() const {
+    return votosLegenda;
+}
+
+int Votos::getVotosNominais() const {
+    return votosNominais;
+}
+
+int Votos::getVotosTotais() const {
+    return votosTotais;
+}
+
+void Votos::setVotosLegenda(int votos) {
+    this->votosLegenda = votos;
+    this->votosTotais = this->votosLegenda + this->votosNominais;
+}
+
+void Votos::setVotosNominais(int votos) {
+    this->votosNominais = votos;
+    this->votosTotais = this->votosLegenda + this->votosNominais;
+}
