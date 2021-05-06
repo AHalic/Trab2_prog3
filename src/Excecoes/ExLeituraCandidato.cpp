df56df6b69bd14e43f90699d0bc7f481b496a3c7
bea <beamaia.work@gmail.com>
2021-05-06 16:04:09 -0300

    exceção de candidato

---
+++ src/Excecoes/ExLeituraCandidato.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+#include "../../include/Excecoes/ExLeituraCandidato.h"
+
+using namespace std;
+  
+ExLeituraCandidato::ExLeituraCandidato(const string &msg) : _msg(msg) {
+    this->_msg = "Erro encontrado na leitura de candidatos!\n" + msg;
+    this->_linha = -1;
+}
+
+ExLeituraCandidato::ExLeituraCandidato(const string &msg, const int &linha) : _msg(msg) {
+    this->_msg = "Erro encontrado na leitura de candidatos!\n" + msg;
+    this->_linha = linha;
+}
+
+ExLeituraCandidato::ExLeituraCandidato() {
+    this->_msg = "Erro encontrado na leitura de candidatos!";
+    this->_linha = -1;
+}
+
+const char * ExLeituraCandidato::what() const throw () {
+    string out = _msg;
+    
+    if (_linha > -1) {
+        out += "\nProblema na linha: " + to_string(this->_linha);
+    }
+
+    return out.c_str();
+}
